e405366afeaf2f13a880099c530cd847b32d6462
airRnot1106 <yngw.rintaro@gmail.com>
2024-02-02 15:59:36 +0900

    feat: add prettyJSON middleware to app

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,9 @@
 import { api } from '@/server/api/route';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { prettyJSON } from 'hono/pretty-json';
 
-const app = new Hono().route('/', api);
+const app = new Hono().use('*', prettyJSON()).route('/', api);
 export type AppType = typeof app;
 
 const port = 3000;
